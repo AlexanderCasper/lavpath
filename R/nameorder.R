47dd02e3e17625b4bd0af4bdc29994ffa30d8465
DrFlausch <casper-trash@web.de>
2020-10-07 17:19:25 +0200

    initial commit

--- R/nameorder.R
+++ R/nameorder.R
@@ -1,8 +1,7 @@
 nameorder <- function(model) {
-
 neu <-  dplyr::select(dplyr::filter(lavaan::parameterEstimates(model, standardized=TRUE),op == "~"),'Latent Factor'=lhs, Indicator=rhs, B=est, SE=se, Z=z, pvalue=pvalue, Beta=std.all)
-neu[4:8] <- sapply(neu[4:8], round, 3)
-neu2 <- neu[2:8]
+neu[4:7] <- sapply(neu[4:7], round, 3)
+neu2 <- neu[2:7]
 neu$Indicator <- NULL
 names(neu)[1] <- "Indicator"
 neu3 <- rbind(neu,neu2)
